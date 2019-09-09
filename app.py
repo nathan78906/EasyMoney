31c8ac4d347deeec7b76a40036d38da1b6c7161f
nathan78906 <nathan.seebarran@gmail.com>
2019-09-09 02:43:59 -0400

    user friendly messages

--- app.py
+++ app.py
@@ -1,30 +1,99 @@
-from datetime import date
+import requests
+import json
+import datetime
+from messenger import sendText
+from whatsapp import sendToWhatsApp
 
-from gs_quant.data import Dataset
-from gs_quant.markets.securities import SecurityMaster, AssetIdentifier
-from gs_quant.session import GsSession
+auth_data = {
+    "grant_type"    : "client_credentials",
+    "client_id"     : "f16cd82ee1114e9aa586d8a9b9061e5d",
+    "client_secret" : "51561554100c3a879a77999c21bae8fbec811421c3f33ccf4ffc562658d3498e",
+    "scope"         : "read_product_data"
+}
 
-client_id = 'f16cd82ee1114e9aa586d8a9b9061e5d'
-client_secret = '51561554100c3a879a77999c21bae8fbec811421c3f33ccf4ffc562658d3498e'
+portfolio = {
+    '213305': {'ticker': 'TSLA', 'name': 'Tesla Inc', 'score': 0, 'number_bought': 1},
+    '149756': {'ticker': 'NFLX', 'name': 'Netflix Inc', 'score': 0, 'number_bought': 1},
+    '227284': {'ticker': 'FB', 'name': 'Facebook Inc-Class A', 'score': 0, 'number_bought': 1},
+    '176665': {'ticker': 'EXPE', 'name': 'Expedia Group Inc', 'score': 0, 'number_bought': 1},
+    '75573': {'ticker': 'ODP', 'name': 'Office Depot Inc', 'score': 0, 'number_bought': 1},
+    '86196': {'ticker': 'ULTI', 'name': 'Ultimate Software Group Inc', 'score': 0, 'number_bought': 1},
+    '25022': {'ticker': 'CMCSA', 'name': 'Comcast Corp-Class A', 'score': 0, 'number_bought': 1},
+    '44644': {'ticker': 'ADP', 'name': 'Automatic Data Processing', 'score': 0, 'number_bought': 1},
+    '66384': {'ticker': 'WDC', 'name': 'Western Digital Corp', 'score': 0, 'number_bought': 1},
+    '86356': {'ticker': 'EBAY', 'name': 'Ebay Inc', 'score': 0, 'number_bought': 1},
+    '161467': {'ticker': 'CRM', 'name': 'Salesforce.com Inc', 'score': 0, 'number_bought': 1}
+}
+gsids = [gsid for gsid in portfolio]
 
-# log in to Marquee
-scopes = GsSession.Scopes.get_default()
-GsSession.use(client_id=client_id, client_secret=client_secret, scopes=scopes)
+SELL_FACTOR = -0.001
+BUY_FACTOR = 0.001
 
-# retrieve data for some GSIDs within a date range 
-ds = Dataset('USCANFPP_MINI')
+# create session instance
+session = requests.Session()
 
-# get a list of covered GSIDs
-gsids = ds.get_coverage()['gsid'].values.tolist()
-data = ds.get_data(date(2017, 1, 15), date(2018, 1, 15), gsid=gsids[0:5])
+auth_request = session.post("https://idfs.gs.com/as/token.oauth2", data = auth_data)
+access_token_dict = json.loads(auth_request.text)
+access_token = access_token_dict["access_token"]
 
-# peek at first few rows of data 
-print(data.head())
+# update session headers with access token
+session.headers.update({"Authorization":"Bearer "+ access_token})
 
-# retrieve asset metadata using Securities Master
-for idx, row in data.iterrows():
-	marqueeAssetId = row['assetId']
-	asset = SecurityMaster.get_asset(marqueeAssetId, AssetIdentifier.MARQUEE_ID)
-	data.loc[data['assetId'] == marqueeAssetId, 'assetName'] = asset.name
-print(data.head())
+request_url = "https://api.marquee.gs.com/v1/data/USCANFPP_MINI/query"
 
+start_date = datetime.datetime(2017, 1, 1)
+end_date = datetime.datetime(2017, 1, 2)
+
+
+for i in range(0, 100):
+    print("{} - {}".format(start_date, end_date))
+    user_input = input("Press enter to continue (or q to quit): ")
+    if user_input == 'q':
+        exit()
+    request_query = {
+                    "where": {
+                        "gsid": gsids
+                    },
+                    "startDate": start_date.strftime("%Y-%m-%d"),
+                    "endDate": end_date.strftime("%Y-%m-%d")
+               }
+
+    request = session.post(url=request_url, json=request_query)
+    results = json.loads(request.text)
+
+    send_to_user = ""
+    buying = ""
+    selling = ""
+
+    for stock in results['data']:
+        net = stock['financialReturnsScore'] - portfolio[stock['gsid']]['score']
+        if portfolio[stock['gsid']]['score'] == 0:
+            portfolio[stock['gsid']]['score'] = stock['financialReturnsScore']
+            send_to_user = "\nThank you for using our automated investment system. We will keep you notified about changes to your investment portfolio."
+        elif net > BUY_FACTOR:
+            if portfolio[stock['gsid']]['number_bought'] < 1:
+                buying += "{} ({})\n".format(portfolio[stock['gsid']]['ticker'], portfolio[stock['gsid']]['name'])
+                portfolio[stock['gsid']]['score'] = stock['financialReturnsScore']
+                portfolio[stock['gsid']]['number_bought'] += 1
+        elif net < SELL_FACTOR:
+            if portfolio[stock['gsid']]['number_bought'] > 0:
+                selling += "{} ({})\n".format(portfolio[stock['gsid']]['ticker'], portfolio[stock['gsid']]['name'])
+                portfolio[stock['gsid']]['score'] = stock['financialReturnsScore']
+                portfolio[stock['gsid']]['number_bought'] -= 1
+        else:
+            portfolio[stock['gsid']]['score'] = stock['financialReturnsScore']
+
+    if buying:
+        send_to_user += "\nWe've found the following stocks to have a higher financial return so they've been added to your portfolio: \n"
+        send_to_user += buying
+    if selling:
+        send_to_user += "\nOh No! Looks like the following stocks have a low financial outlook. We've temporarily removed them from your portfolio: \n"
+        send_to_user += selling.rstrip("\n")
+
+    if send_to_user:
+        send_to_user = start_date.strftime("%Y-%m-%d") + "\n" + send_to_user
+        print(send_to_user)
+        # sendText(send_to_user)
+        sendToWhatsApp(send_to_user)
+    start_date += datetime.timedelta(days=1)
+    end_date += datetime.timedelta(days=1)
